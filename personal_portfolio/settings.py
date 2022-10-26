312525a71f20de93249c3dec521e66ca77adca1a
Cergo3301 <cergo3301@yandex.ru>
2022-10-26 14:03:16 +0000

    remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@@-#&zutca21+18uccw_3u*y7%b2t^k7r6aw$$1sz58g==g+&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['cergo3301.pythonanywhere.com']
 
 
 # Application definition
@@ -107,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
